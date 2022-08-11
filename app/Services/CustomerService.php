d6a2821ade50d5ce004e44f785418d5a29310a16
sadats90 <sadatshahriar.ss@gmail.com>
2022-08-12 00:43:54 +0600

    crud without update

--- app/Services/CustomerService.php
+++ app/Services/CustomerService.php
@@ -77,6 +77,7 @@ class CustomerService
         $validator = Validator::make($data, [
             'title' => 'bail|min:2',
             'description' => 'bail|max:255'
+           
         ]);
 
         if ($validator->fails()) {
@@ -87,8 +88,11 @@ class CustomerService
 
         try {
             $customer = Customer::find($id);
+        
 
             $customer->update($data);
+
+
             $genInfo = [];
             if(isset($data['abn']))
             {
@@ -102,52 +106,52 @@ class CustomerService
             {
                 $genInfo['account_no'] = $data['account_no'];
             }
-            // if(isset($data['data']))
-            // {
-            //     $genInfo['data'] = $data['data'];
-            // }
-            // $genInfo = [
-            //     // 'abn' => $data['abn'],
-            //     // 'tfn' => $data['tfn'],
-            //     // 'bsb' => $data['bsb'],
-            //     // 'account_no' => $data['account_no'],
-            //     // 'spouse_name' => $data['spouse_name'],
-            //     // 'spouse_date_of_birth' => $data['spouse_date_of_birth'],
-            //     // 'no_of_dependants' => $data['no_of_dependants'],
-            //     // 'spouse_income' => $data['spouse_income'],
-            //     // 'have_log_book' => $data['have_log_book'],
-            //     // 'model' => $data['model'],
-            //     // 'rego' => $data['rego'],
-            //     // 'kms' => $data['kms'],
-            //     // 'work_related_travel_expense' => $data['work_related_travel_expense'],
-            //     // 'work_related_travel_expense_amount' => $data['work_related_travel_expense_amount'],
-            //     // 'uniform' => $data['uniform'],
-            //     // 'shoes' => $data['shoes'],
-            //     // 'laundry' => $data['laundry'],
-            //     // 'work_related_self_education_expense' => $data['work_related_self_education_expense'],
-            //     // 'work_related_self_education_expense_amount' => $data['work_related_self_education_expense_amount'],
-            //     // 'mobile_phone_business' => $data['mobile_phone_business'],
-            //     // 'mobile_phone_amount' => $data['mobile_phone_amount'],
-            //     // 'internet_business' => $data['internet_business'],
-            //     // 'internet_amount' => $data['internet_amount'],
-            //     // 'computer_expense_business' => $data['computer_expense_business'],
-            //     // 'computer_expense_amount' => $data['computer_expense_amount'],
-            //     // 'seminar_printing_books_business' => $data['seminar_printing_books_business'],
-            //     // 'seminar_printing_books_amount' => $data['seminar_printing_books_amount'],
-            //     // 'tools_business' => $data['tools_business'],
-            //     // 'tools_amount' => $data['tools_amount'],
-            //     // 'license_business' => $data['license_business'],
-            //     // 'license_amount' => $data['license_amount'],
-            //     // 'union_fees_business' => $data['union_fees_business'],
-            //     // 'union_fees_amount' => $data['union_fees_amount'],
-            //     // 'overtime_business' => $data['overtime_business'],
-            //     // 'overtime_amount' => $data['overtime_amount'],
-            //     // 'others' => $data['others'],
-            //     // 'others_business' => $data['others_business'],
-            //     // 'others_amount' => $data['others_amount']
+            if(isset($data['data']))
+            {
+                $genInfo['data'] = $data['data'];
+            }
+        //     $genInfo = [
+        //         'abn' => $data['abn'],
+        //         'tfn' => $data['tfn'],
+        //         'bsb' => $data['bsb'],
+        //         'account_no' => $data['account_no'],
+        //         'spouse_name' => $data['spouse_name'],
+        //         'spouse_date_of_birth' => $data['spouse_date_of_birth'],
+        //         'no_of_dependants' => $data['no_of_dependants'],
+        //         'spouse_income' => $data['spouse_income'],
+        //         'have_log_book' => $data['have_log_book'],
+        //         'model' => $data['model'], 
+        //         'rego' => $data['rego'],
+        //         'kms' => $data['kms'],
+        //         'work_related_travel_expense' => $data['work_related_travel_expense'],
+        //         'work_related_travel_expense_amount' => $data['work_related_travel_expense_amount'],
+        //         'uniform' => $data['uniform'],
+        //         'shoes' => $data['shoes'],
+        //         'laundry' => $data['laundry'],
+        //         'work_related_self_education_expense' => $data['work_related_self_education_expense'],
+        //         'work_related_self_education_expense_amount' => $data['work_related_self_education_expense_amount'],
+        //         'mobile_phone_business' => $data['mobile_phone_business'],
+        //         'mobile_phone_amount' => $data['mobile_phone_amount'],
+        //         'internet_business' => $data['internet_business'],
+        //         'internet_amount' => $data['internet_amount'],
+        //         'computer_expense_business' => $data['computer_expense_business'],
+        //         'computer_expense_amount' => $data['computer_expense_amount'],
+        //         'seminar_printing_books_business' => $data['seminar_printing_books_business'],
+        //         'seminar_printing_books_amount' => $data['seminar_printing_books_amount'],
+        //         'tools_business' => $data['tools_business'],
+        //         'tools_amount' => $data['tools_amount'],
+        //         'license_business' => $data['license_business'],
+        //         'license_amount' => $data['license_amount'],
+        //         'union_fees_business' => $data['union_fees_business'],
+        //         'union_fees_amount' => $data['union_fees_amount'],
+        //         'overtime_business' => $data['overtime_business'],
+        //         'overtime_amount' => $data['overtime_amount'],
+        //         'others' => $data['others'],
+        //         'others_business' => $data['others_business'],
+        //         'others_amount' => $data['others_amount']
 
 
-            // ];
+        //     ];
 
 
 
@@ -176,9 +180,11 @@ class CustomerService
     public function save($payload)
     {
 
-
+        
         $customer = Customer::Create($payload);
-        $customerGeneralInfo =$customer->customer_general_info()->Create($payload);
+        
+        $customerGeneralInfo = $customer->customer_general_info()->Create($payload);
+
         return;
     }
 
